5a309910c94049e717a1a8ffc3d70e320f3f2f49
ef4eb856 <ef4eb856@opayq.com>
2019-12-14 14:42:19 -0500

    Initial Commit

---
+++ GRIT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GRIT
+//
+//  Created by P on 12/14/19.
+//  Copyright © 2019 Cincinnatus Farms Ltd Co. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
